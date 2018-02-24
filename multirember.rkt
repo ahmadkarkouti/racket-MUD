6a82ea928fec11abe4d2e7e63689df89492171c7
Ahmad Karkouty <21269015@student.uwl.ac.uk>
2018-02-24 05:25:43 +0000

    Version 0.0.1

---
+++ multirember.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((eq? ( car lat) a)
+          ( multirember a ( cdr lat)))
+         (else ( cons ( car lat)
+                      ( multirember a
+                                    ( cdr lat))))))))) 
