7387016919b8ac0bf23729352e4aefab5fe8ddb8
Ahmad Karkouty <21269015@student.uwl.ac.uk>
2018-02-24 05:40:50 +0000

    Version 0.0.5

--- multiinsertL.rkt
+++ multiinsertL.rkt
@@ -1,5 +1,16 @@
 #lang racket
 
 (define multiinsertL
-(lambda (new old lat)
-(cond 
+  (lambda ( new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ( ( eq? ( car lat) old)
+           ( cons new
+                  ( cons old
+                         ( multiinsertL new old
+                                        lat))))
+         (else ( cons ( car lat)
+                      ( multiinsertL new old
+                                     ( cdr lat)))))))))
