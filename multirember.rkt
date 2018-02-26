5a4ed2cf5efcd6af642f03c42c07722805c1769a
Ahmad Karkouty <21269015@student.uwl.ac.uk>
2018-02-26 06:29:54 +0000

    Version 0.1.2

--- multirember.rkt
+++ multirember.rkt
@@ -1,13 +1,28 @@
 #lang racket
 
-(define multirember
-  (lambda (a lat)
-    (cond
-      ((null? lat) (quote ()))
+;TASK 1: MULTIREMBER
+;----------------------
+;----------------------
+(define multirember ;This is defining the function multirember
+  (lambda (a lat)   ;Lambda is an anonymous function
+    
+    (cond           ;Condition
+      ((null? lat) (quote ())) ;Check if lat is null
       (else
-       (cond
-         ((eq? ( car lat) a)
+       (cond                   ;Condition
+         ((eq? ( car lat) a)   ; if (car lat) = a
+
+          ;if the first atom of the list == a
           ( multirember a ( cdr lat)))
+          ;Jump back to multirember and remove the first atom of the list
+
+         
          (else ( cons ( car lat)
+          ;Else construct a new list with every atom that is not equal to a
                       ( multirember a
-                                    ( cdr lat))))))))) 
+                                    ( cdr lat)))))))))
+
+;Examples to test the program
+;-----------------------------
+;(multirember 'blue '(red blue white blue green blue black yellow blue))
+;'(red white green black yellow)
