8b6ba747f829c4998752436afc1faf5fd140d456
Ahmad Karkouty <21269015@student.uwl.ac.uk>
2018-02-26 07:08:02 +0000

    Version 0.1.3

---
+++ rember.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+
+
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? ( car lat) a) ( cdr lat))
+      (else ( cons ( car lat)
+                   (rember a ( cdr lat))))))) 
