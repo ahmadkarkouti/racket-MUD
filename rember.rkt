04dea47b1f39c60596545f778d7672eae9a2f6ed
Ahmad Karkouty <21269015@student.uwl.ac.uk>
2018-02-26 07:15:26 +0000

    Version 0.1.4

--- rember.rkt
+++ rember.rkt
@@ -1,11 +1,22 @@
 #lang racket
 
+;REMBER
+;--------
+;--------
+(define rember ;This is defining the main function rember
+  (lambda (a lat);Lambda is an anonymous function
+    
+    (cond ;Condition
+      
+      ((null? lat) (quote ())) ;Check if lat is null
+      ((eq? ( car lat) a) ( cdr lat)) ;check if (car lat) = a = (cdr lat)
+
+      ;Else construct a new list excluding the first atom that is equal to a
+      (else ( cons ( car lat) 
+                   (rember a ( cdr lat)))))))
 
 
-(define rember
-  (lambda (a lat)
-    (cond
-      ((null? lat) (quote ()))
-      ((eq? ( car lat) a) ( cdr lat))
-      (else ( cons ( car lat)
-                   (rember a ( cdr lat))))))) 
+;Examples to test the program
+;-----------------------------
+;(rember 'blue '(red blue white blue green blue black yellow blue))
+;'(red white blue green blue black yellow blue)
