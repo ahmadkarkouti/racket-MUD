361873e75170c70ad176a09bd192c113613dfe55
Ahmad Karkouty <21269015@student.uwl.ac.uk>
2018-03-12 23:13:50 +0000

    Version 0.0.2

--- assignment2/dungeon game.rkt
+++ assignment2/dungeon game.rkt
@@ -1,18 +1,58 @@
 #lang racket
+(require racket/gui)
+(require racket/draw)
+(require racket/mpair)
+(require rackunit)
+(require rackunit/text-ui)
+(struct pos (x y))
 
-(require srfi/1)
-(require srfi/13)
-(require srfi/48)
 
-(define objects '((1 "a steel sword")
-                  (1 "a loaf of bread")
-                  (1 "a torch")))
+(define width 64)
+(define height 47)
+(define done 16)
+(define screen_width (* width done))
+(define screen_height (* height done))
 
-(define places '((1 "You are in the the Hallway")
-                 (2 "You are in the Chapel")
-                 (3 "You are in the Armory")
-                 (4 "You are in the Summoning room")))
 
-(define examine '(((directions) look) ((look) look) ((examine room) look)))
-(define exit '(((exit game) quit) ((quit game) quit) ((exit) quit) ((quit) quit)))
-(define actions '(,@examine ,@exit))
+(define background (read-bitmap "./dungeon.jpg"))
+
+(define frame (new frame%
+                   [label "Ahmad's Dungeon Game"]
+                   [width screen_width]
+                   [height screen_height])
+)
+
+
+(define (draws-sprite sprite poss)
+  (send dc draw-bitmap sprite (pos-x  poss) (pos-y poss))
+)
+
+(define (canvas-key frame) (class canvas%
+                             (super-new [parent frame])))
+
+(define canvas ( new (canvas-key frame)))
+(define dc (send canvas get-dc))
+(send frame show #t)
+
+
+(define my-text-field%
+  (class text-field%
+    (super-new)
+    (define/override (on-focus on?)
+      (when on? (printf "~a\n" (send this get-label))))))
+
+(define field-1 (new my-text-field% [label "Command"] [parent frame]))
+
+(send frame show #t)
+(define timer (new timer%
+                   [notify-callback (lambda()
+                                      (draws-sprite background (pos 0 0))
+                                      ;(message-box "Title" "Do you wish to continue?" #f '(yes-no))
+
+)]))
+
+
+(define start (lambda()
+                  (send timer start 70)))
+
+(start)
